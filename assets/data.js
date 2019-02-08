0a92cdd25e6b76d9a16a28368f41e0f5fe3faccd
nguye <nguyenv2@outlook.com>
2019-02-08 14:43:58 -0800

    layout added

--- assets/data.js
+++ assets/data.js
@@ -6,7 +6,7 @@ var data = [
     },
     {
         title: 'ownership',
-        explanation: 'Leaders are owners. They think long term and don’t sacrifice long-term value for short-term results. They act on behalf of the entire company, beyond just their own team. They never say "that’s not my job ".'
+        explanation: 'Leaders are owners. They think long term and don\'t sacrifice long-term value for short-term results. They act on behalf of the entire company, beyond just their own team. They never say "that\'s not my job ".'
     },
     {
         title: 'invent and simplify',
