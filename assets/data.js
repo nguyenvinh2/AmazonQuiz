ba7f2496c711e7e77d79a41b0852b925449ae72b
nguye <nguyenv2@outlook.com>
2019-02-08 14:30:37 -0800

    eslint clean up

--- assets/data.js
+++ assets/data.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 var data = [
     {
         title: 'customer obsession',
--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
 'use strict';
 let count = 0
 
-$('.flip-card .flip-card-inner .flip-card-back p').text(data[5].explanation);
-
-
+// eslint-disable-next-line no-undef
 data.forEach(function(item){
     $('.container').append(`<div class="flip-card ${count}"></div>`);
     $(`.flip-card.${count}`).append(`<div class="flip-card-inner"></div>`);
