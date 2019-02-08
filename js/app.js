d8da6909e8c3bccc394cc482642b1c83be8a27b8
nguye <nguyenv2@outlook.com>
2019-02-08 14:25:31 -0800

    for each rendering

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 'use strict';
+let count = 0
 
 $('.flip-card .flip-card-inner .flip-card-back p').text(data[5].explanation);
+
+
+data.forEach(function(item){
+    $('.container').append(`<div class="flip-card ${count}"></div>`);
+    $(`.flip-card.${count}`).append(`<div class="flip-card-inner"></div>`);
+    $(`.flip-card.${count} .flip-card-inner`).append(`<div class="flip-card-front">${item.title}</div>`);
+    $(`.flip-card.${count} .flip-card-inner`).append(`<div class="flip-card-back">${item.explanation}</div>`);
+    count++;
+});
