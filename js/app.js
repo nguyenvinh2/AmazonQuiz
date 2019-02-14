315e80890f828ed5074e08128b8e5f68f7e8feca
nguye <nguyenv2@outlook.com>
2019-02-14 15:49:50 -0800

    lower case fix

--- js/app.js
+++ js/app.js
@@ -2,6 +2,13 @@
 let count = 0;
 let score = 0;
 
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName('close')[0];
+
 // eslint-disable-next-line no-undef
 data.forEach(function (item) {
     $('.container').append(`<div class="flip-card ${count}"></div>`);
@@ -16,7 +23,7 @@ function revealAnswer(e) {
     count = 0;
     // eslint-disable-next-line no-undef
     data.forEach(function (item) {
-        if (item.title.toLowerCase() === $('#question input[name="answer"]').val()) {
+        if (item.title.toLowerCase() === $('#question input[name="answer"]').val().toLowerCase()) {
             console.log(item.title.toLowerCase());
             $(`.${count} p`).removeClass('hidden');
             score = 14 - $('.hidden').length;
@@ -33,3 +40,18 @@ $('input[name=answer]').on('keydown', function(e) {
         e.preventDefault();
     }
 });
+function show(){
+    modal.style.display = 'block';
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = 'none';
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+}
